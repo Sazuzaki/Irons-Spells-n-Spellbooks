eb503c839849afcd5e69ea5ee18ef4ddef7113d3
Sazuzaki <sazuzaki@gmail.com>
2023-10-05 19:13:04 -0500

    Update RaiseDeadSpell.java

--- src/main/java/io/redspace/ironsspellbooks/spells/blood/RaiseDeadSpell.java
+++ src/main/java/io/redspace/ironsspellbooks/spells/blood/RaiseDeadSpell.java
@@ -55,7 +55,7 @@ public class RaiseDeadSpell extends AbstractSpell {
         this.manaCostPerLevel = 10;
         this.baseSpellPower = 10;
         this.spellPowerPerLevel = 3;
-        this.castTime = 30;
+        this.castTime = 60;
         this.baseManaCost = 50;
 
     }
